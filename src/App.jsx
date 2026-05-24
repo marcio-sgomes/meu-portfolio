7f1d4b4769f60182a05013a75872b44f4d470183
Márcio Gomes <marcio.sant.gomes@gmail.com>
2026-05-24 14:17:44 -0300

    feat: configuracao inicial do react com tailwind 4

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+<h1 className="text-4xl font-bold text-blue-600">Meu Portfólio Profissional</h1>      <p>Bem-vindo ao meu portfólio! Aqui você encontrará meus projetos e experiências.</p>
+    </div>
+  );
+}
+
+export default App;
